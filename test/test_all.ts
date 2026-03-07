0fe780f7f78b71798dbeefe7f9b2b45826a5c3c9
Tadashi <kobayasi.tadashi@gmail.com>
2026-03-07 13:54:56 +0100

    clean files for refactoring

--- test/test_all.ts
+++ test/test_all.ts
@@ -7,24 +7,6 @@ import { spawnSync } from 'child_process';
 import * as path from 'path';
 
 const testFiles = [
-  'test_database_separation.ts',
-  'test_definition_provider.ts',
-  'test_goto_definition.ts',
-  'test_module_update.ts',
-  'test_symbols.ts',
-  'test_antlr_parser.ts',
-  'test_antlr_symbols.ts',
-  'test_parser.ts',
-  'test_integration.ts',
-  'test_signal_warnings.ts',
-  'test_instance_database.ts',
-  'test_startup_scan.ts',
-  'test_parameter_database.ts',
-  'test_hdlignore.ts',
-  'test_preprocessor.ts',
-  'test_semantic_tokens.ts',
-  'test_port_hover.ts',
-  'test_parse_modules.ts'
 ];
 
 const total = testFiles.length;
