af72c5faad7060c21b3be3e89c4a7e02df311abc
Tadashi <kobayasi.tadashi@gmail.com>
2026-03-14 12:28:28 +0100

    test include directive

--- contents/test_directives.v
+++ contents/test_directives.v
@@ -13,11 +13,19 @@ module test_directives (
     output reg [`BUS_WIDTH-1:0] data_out
 );
 
+    reg[`ADDR_WIDTH-1:0] addr;
+
     always @(posedge clk) begin
         if (reset)
             data_out <= 0;
         else
             data_out <= data_in;
+
+        if (reset)
+            addr <= data_in[`ADDR_WIDTH-1:0];
+        else
+            addr <= addr + 1;
+        
     end
 
 endmodule
